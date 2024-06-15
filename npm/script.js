7b7c97fc5735132dbdc97bc6642a6a343ccf3fb5
TheRockMilad <therock.milad2@outlook.com>
2024-06-15 11:31:23 +0330

    export and import

---
+++ npm/script.js
@@ -0,0 +1,6 @@
+// const {username} = require('./utils')
+import num,{double, username} from "./utils.js"
+console.log(username);
+
+console.log(num);
+console.log(double(10));
---
+++ npm/utils.js
@@ -0,0 +1,9 @@
+let username = "Milad";
+
+function double(num) {
+  return num * 2;
+}
+let num = 50;
+
+export default num;
+export { username, double };
