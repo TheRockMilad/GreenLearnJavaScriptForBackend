8da6e609a7e022d1cf72075741bb6e2f4621bf76
TheRockMilad <therock.milad2@outlook.com>
2024-06-22 12:11:57 +0330

    added dotenv package

--- mongodb/index1.js
+++ mongodb/index1.js
@@ -1,29 +1,37 @@
 const { MongoClient } = require("mongodb");
-const url = "mongodb://127.0.0.1:27017";
-const dbconnection = new MongoClient(url);
-const dbName = "library";
+const http = require("http");
+//.env اینجور این فایل کانفیگ میشه
+require("dotenv").config();
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    console.log("Home");
+  }
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end();
+});
+
+// میشه اینجا ازش استفاده کرد
+server.listen(process.env.PORT, () => {
+  console.log(`Server Running On Port ${process.env.PORT}`);
+});
+
+const dbconnection = new MongoClient(process.env.dbConnectionUrl);
+const dbName = process.env.dbName;
 
 const main = async () => {
   await dbconnection.connect();
   console.log("Connected to dbConnection succesfully");
   const db = dbconnection.db(dbName);
 
-  //اضافه کردن یک داکیومنت به دیتابیس
-  //   const userCollectiion = db.collection("Users");
-  //   userCollectiion.insertOne({
-  //     name: "Milad",
-  //     username: "smh",
-  //     eamil: "therock@gmail.com",
-  //     crime: 0,
-  //     role: "ADMIN",
-  //   });
-
-  //اضافه کردن چند داکیومنت به دیتابیس
-  //   const rentCollection = db.collection("rent");
-  //   rentCollection.insertMany([
-  //     { userID: 1, bookId: 1 },
-  //     { userID: 2, bookId: 2 },
-  //   ]);
+//   const userCollectiion = db.collection("Users");
+//   userCollectiion.insertOne({
+//     name: "Hamid",
+//     username: "HHH",
+//     eamil: "Hamid@gmail.com",
+//     crime: 0,
+//     role: "ADMIN",
+//   });
 
   return "done";
 };
