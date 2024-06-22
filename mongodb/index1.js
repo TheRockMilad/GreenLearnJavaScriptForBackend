e6b6bdb7bb11f5ae0361f1b8d5621568b1b7ab66
TheRockMilad <therock.milad2@outlook.com>
2024-06-22 16:00:01 +0330

    Drop collection and database

--- mongodb/index1.js
+++ mongodb/index1.js
@@ -88,21 +88,30 @@ const main = async () => {
   //   console.log(mainUser.address.city);
 
   // ------------------- operators------------------------
-  const result = await userCollectiion
-    .find({
-      // role : {$ne : "USER"}      نباشه
-      // role : {$eq : "USER"}      باشه
-      // crime : {$lt : 12}         crime > 12
-      // crime : {$lte : 12}        crime >= 12
-      // crime : {$gt : 12}         crime < 12
-      // crime : {$gte : 12}        crime <= 12
-      // crime : {$in : [12,5]}     include
-      // crime : {$nin : [12,5]}    not in
-      //   $or: [{ crime: { $in: [22, 12] } }, { age: 18 }],  یا
-    })
-    .toArray();
+//   const result = await userCollectiion
+//     .find({
+//       // role : {$ne : "USER"}      نباشه
+//       // role : {$eq : "USER"}      باشه
+//       // crime : {$lt : 12}         crime > 12
+//       // crime : {$lte : 12}        crime >= 12
+//       // crime : {$gt : 12}         crime < 12
+//       // crime : {$gte : 12}        crime <= 12
+//       // crime : {$in : [12,5]}     include
+//       // crime : {$nin : [12,5]}    not in
+//       //   $or: [{ crime: { $in: [22, 12] } }, { age: 18 }],  یا
+//     })
+//     .toArray();
 
-  console.log(result);
+//   console.log(result);
+
+//-----------------Drop Collection and Database---------------------
+// پاک کردن کالکشن ها
+// const result = await userCollectiion.drop()
+// const result = await db.collection("books").drop()
+// const result = await db.dropCollection('rents')
+
+// پاک کردن کل دیتابیس
+// const result = await db.dropDatabase()
 
   return "done";
 };
