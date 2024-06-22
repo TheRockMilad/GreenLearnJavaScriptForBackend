8c65992eaaac95148622fc1b2ba15d309ee792fe
TheRockMilad <therock.milad2@outlook.com>
2024-06-22 14:39:04 +0330

    Replace and Nested Document

--- mongodb/index1.js
+++ mongodb/index1.js
@@ -54,14 +54,38 @@ const main = async () => {
   // این متد ، هرچیزی رو که پاک میکنه تو خروجی ذخیره میکنه
 
   //Update
-  const updateUser = await userCollectiion.updateOne(
-    { name: "Milad" },
-    {
-      $set: {
-        crime: 120000,
-      },
-    }
-  );
+  //   const updateUser = await userCollectiion.updateOne(
+  //     { name: "Milad" },
+  //     {
+  //       $set: {
+  //         crime: 120000,
+  //       },
+  //     }
+  //   );
+
+  //replace && nasted Document----------------------------------
+  //   const replaceUser = await userCollectiion.replaceOne(
+  //     { name: "Milad" },
+  //     {
+  //       name: "Milad",
+  //       family : "Hosseini",
+  //       username : "Therock_milad",
+  //       age: 32,
+  //       tags: ["Soccer Player", "Backend Dev", "Gamer"],
+  //       address: {
+  //         country: "Iran",
+  //         province: "Tehran",
+  //         city: "Tehran",
+  //       },
+  //     }
+  //   );
+
+  //   console.log(replaceUser);
+
+  const mainUser = await userCollectiion.findOne({
+    _id: new ObjectId("66766cd4d40f674f5612b2f2"),
+  });
+  console.log(mainUser.address.city);
 
   return "done";
 };
